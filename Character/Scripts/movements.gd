9edc8c27a6e198b228bb25720625234f13b134fc
Radhamante <fafel130@gail.com>
2024-01-20 01:53:27 +0400

    feat : player movement and interactions

--- Character/Scripts/movements.gd
+++ Character/Scripts/movements.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
-@export var walk_speed = 4.0
-const TILE_SIZE = 16
+@export var TILE_SIZE = 16
+@export var walk_speed = 50.0
+
+var lineEditFocused = false
+
+@onready var ray = $RayCast2D 
 
 var initial_position = position
 var input_direction = Vector2.ZERO
+var last_input_direction = Vector2.ZERO
 var is_moving = false
 var percent_moved_to_next_tile = 0.0
 
 @onready var animation = $AnimatedSprite2D
 
 func _physics_process(delta):
-	if is_moving == false:
-		animation.play("idle")
+	if not lineEditFocused:
 		process_player_movement_input()
-	elif input_direction != Vector2.ZERO:
-		move(delta)
-	else:
-		is_moving = false
+		if input_direction != Vector2.ZERO:
+			last_input_direction = input_direction
+			move(delta)
 
 func process_player_movement_input():
 	if input_direction.y == 0:
@@ -32,44 +35,22 @@ func process_player_movement_input():
 			animation.play("up")
 		elif Input.is_action_pressed("down"):
 			animation.play("down")
-	
-	
-	if input_direction != Vector2.ZERO:
-		initial_position = position
-		is_moving = true
 		
 func move(delta):
-	percent_moved_to_next_tile += walk_speed * delta
-	if percent_moved_to_next_tile >= 1.0:
-		position = initial_position + (input_direction * TILE_SIZE)
-		percent_moved_to_next_tile = 0.0
-		is_moving = false
-	else:
-		position = initial_position + (input_direction * TILE_SIZE * percent_moved_to_next_tile)
+	velocity = input_direction * walk_speed
+	move_and_slide()
+
+func _on_line_edit_focus_entered():
+	lineEditFocused = true
+
+func _on_line_edit_focus_exited():
+	lineEditFocused = false
 
 
+func _on_line_edit_text_submitted(new_text):
+	var desired_step: Vector2 = last_input_direction * TILE_SIZE
+	ray.target_position = desired_step
+	ray.force_raycast_update()
+	if ray.is_colliding():
+		print(ray.get_collider().interact(new_text))
 
-#@export var MAX_SPEED := 300
-#@export var ACCELERATION :=  1500
-#@export var FRICTION := 1200
-#
-#@onready var axis = Vector2.ZERO
-#
-#func _physics_process(delta):
-	#pass
-	#
-#func get_input_axis():
-	#axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	#axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
-#
-#func move (delta):
-	#axis = get_input_axis()
-	#if axis == Vector2.ZERO:
-		#apply_friction(FRICTION * delta)
-	#else:
-		#apply_novement (axis * ACCELERATION * delta)
-	#move_and_slide()
-#
-#func apply_friction (amount):
-	#if velocity.length() › amount:
-		#
